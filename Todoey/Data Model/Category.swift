2a8c9732ec6fbe674631ebefe3ced467a902e5e0
Victor <vrw1984@gmail.com>
2018-02-21 21:18:56 -0400

    Persist data using Codable protocol and Plist

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Victor on 2018-02-11.
+//  Copyright © 2018 kodechamp. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object{
+    @objc dynamic var name: String = ""
+   
+    //let items = List<Item>()
+}
